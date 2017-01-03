50586de6321a44fff3560d1a9390c1c1147fb0fd
Christian Kniep <christian@qnib.org>
2017-01-03 21:29:05 +0100

    add --rpcapi db,eth,net,web3,personal,shh --shh to geth startup

--- start.sh
+++ start.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-function read_kv {
-    VAL=$(consul-cli --consul=consul:8500 kv read bootnode/${1})
-    if [ "X${VAL}" == "X" ];then
-        sleep 1
-        read_kv ${1}
-    else
-        echo ${VAL}
-    fi
-}
-ENODE_ID=$(read_kv enode)
-ENODE_PORT=$(read_kv port)
-ENODE_HOST=$(read_kv host)
 
-GETH_OPTS="--rpc --rpcaddr 0.0.0.0"
-if [ "X${ET_MINE_ACCOUNT}" != "X" ];then
-    GETH_OPTS="${GETH_OPTS} --mine --minerthreads=1 --etherbase=${ET_MINE_ACCOUNT}"
-fi
-#GETH_OPTS="${GETH_OPTS} --bootnodes=enode://${ENODE_ID}@$(host -t A bootnode |awk '{print $NF}'):30301"
+GETH_OPTS="--rpc --rpcaddr 0.0.0.0 --rpcapi db,eth,net,web3,personal,shh --shh"
 
 geth $(echo ${GETH_OPTS} | tr -d '"')
