b777a652c05676819b5c7c69d029909e420e987e
Christian Kniep <christian@qnib.org>
2017-01-25 16:06:35 +0100

    include the miner module

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ if [ "X${GETH_TEST_DATABASE}" == "Xtrue" ];then
     rm -rf /data/db
     cp -r /opt/db /data/
 fi
-geth --datadir /data/db --nodiscover --port 30301 --rpc --rpcapi db,eth,net,web3,personal,shh --shh --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcport 8545 --ipcpath /root/.ethereum/geth.ipc --ipcapi "db,eth,net,web3" &
+geth --datadir /data/db --nodiscover --port 30301 --rpc --rpcapi db,eth,miner,net,web3,personal,shh --shh --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcport 8545 --ipcpath /root/.ethereum/geth.ipc --ipcapi "db,eth,net,web3,miner" &
 
 if [ "X${GETH_BOOTSTRAP}" == "Xtrue" ];then
     sleep 3
