6644f79a9ceb578de5606825b5db4be252729826
ravs1 <ravishstudent@outlook.com>
2017-01-29 16:47:47 +0100

    Revised Monitor Contract

--- MonitorContract.sol
+++ MonitorContract.sol
@@ -2,22 +2,50 @@ pragma solidity ^0.4.0;
 
 contract monitorContract {
     
-    struct monitorInfo {
-        address buyerAddress;  // could change this into other address
+    struct monitorJob {
+        address serviceAddress;  // could change this into other address
         bytes32 serviceUrl;
+        bool done;
+        uint result;
+        address monitorAddress;
     }
     
-    uint public monitorInfoCount;
+    uint public jobsCount;
     
-    mapping(uint => monitorInfo) public monitorResults;
+    mapping(uint => monitorJob) public monitorJobs;
+    uint []jobsAvailable;
     
     event newMonitorRecord(address buyerAddress, bytes32 url);
     
-    function monitor (address buyerAddress, bytes32 url) returns (uint monitorIndex) {
-        monitorInfoCount += 1;
-        monitorResults[monitorInfoCount].buyerAddress = buyerAddress;
-        monitorResults[monitorInfoCount].serviceUrl = url;
-        newMonitorRecord(buyerAddress, url);
-        return monitorInfoCount;
+    function monitor (address serviceAddress, bytes32 url){
+        jobsCount += 1;
+        monitorJobs[jobsCount] = monitorJob({
+            serviceAddress :serviceAddress,
+            serviceUrl : url, 
+            done : false,
+            result : 0,
+            monitorAddress : address(0)
+            
+        });
+        jobsAvailable.push(jobsCount);
+        newMonitorRecord(serviceAddress, url);
+        
+    }
+
+    event jobMonitorEvent(uint jobIndex);
+    function getMonitorRequest(){
+        if(jobsAvailable.length != 0){
+            uint job = jobsAvailable[0];
+            jobMonitorEvent(job);
+            delete jobsAvailable[0];
+            monitorJobs[job].monitorAddress = msg.sender;
+        }
+    }
+    
+    function saveMonitorResults(uint jobIndex,uint result){
+        if(monitorJobs[jobIndex].monitorAddress == msg.sender){
+            monitorJobs[jobIndex].result = result;
+            monitorJobs[jobIndex].done = true;
+        }
     }
 }
