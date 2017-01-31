12266a7d1b463337801e8a1ddca385c893e1e8ea
Ahmed Tidjani <a.tidjani@campus.tu-berlin.de>
2017-01-31 15:16:10 +0100

    refactor Monitroing contract

--- MonitorContract.sol
+++ MonitorContract.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.0;
 
-contract monitorContract {
+contract Monitor {
     
     struct monitorJob {
         address serviceAddress;  // could change this into other address
@@ -15,9 +15,9 @@ contract monitorContract {
     mapping(uint => monitorJob) public monitorJobs;
     uint []jobsAvailable;
     
-    event newMonitorRecord(address buyerAddress, bytes32 url);
+    event newMonitorRecord();
     
-    function monitor (address serviceAddress, bytes32 url){
+    function submitMonitoringRequest (address serviceAddress, bytes32 url){
         jobsCount += 1;
         monitorJobs[jobsCount] = monitorJob({
             serviceAddress :serviceAddress,
@@ -28,21 +28,21 @@ contract monitorContract {
             
         });
         jobsAvailable.push(jobsCount);
-        newMonitorRecord(serviceAddress, url);
+        newMonitorRecord();
         
     }
 
-    event jobMonitorEvent(uint jobIndex);
-    function getMonitorRequest(){
+    event jobMonitorEvent(address sender,uint jobIndex);
+    function getMonitoringRequest(){
         if(jobsAvailable.length != 0){
             uint job = jobsAvailable[0];
-            jobMonitorEvent(job);
+            jobMonitorEvent(msg.sender,job);
             delete jobsAvailable[0];
             monitorJobs[job].monitorAddress = msg.sender;
         }
     }
     
-    function saveMonitorResults(uint jobIndex,uint result){
+    function saveMonitoringResults(uint jobIndex,uint result){
         if(monitorJobs[jobIndex].monitorAddress == msg.sender){
             monitorJobs[jobIndex].result = result;
             monitorJobs[jobIndex].done = true;
